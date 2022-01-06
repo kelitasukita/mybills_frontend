fab8f7491bada13ed0b6da70008c8e21cb660319
Kelita Lamar <kelitasukita@gmail.com>
2022-01-06 22:03:23 +0100

    I will start again

--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-import { Header } from './components/Header';
-import './styles/global.scss';
-
-export function App() {
-  return <Header />;
-}
--- src/components/ItemMenu.jsx
+++
@@ -1,4 +0,0 @@
-export function ItemMenu(props) {
-
-  return <li>{props.label}</li>;
-}
