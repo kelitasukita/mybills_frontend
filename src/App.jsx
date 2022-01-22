20dcac6f1729bb92a4c5607601de7d24d161f5c4
Kelita Lamar <kelitasukita@gmail.com>
2022-01-22 23:00:33 +0100

    Add credit-card boxes and fix styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import './styles/global.scss'
 import './styles/header.scss'
 import './styles/overviewPanel.scss'
 import './styles/payBoxes.scss'
+import './styles/creditCards.scss'
+import './styles/footer.scss'
 import { Header } from "./components/Header/header";
 import { OverviewPanel } from './components/OverviewPanel/OverviewPanel';
 import { Paid } from './components/Bills/Paid';
 import { ToPay } from './components/Bills/ToPay';
+import { Amex } from './components/CreditCards/Amex';
+import { Footer } from './components/Footer'
 
 export function App() {
   return (
@@ -14,10 +18,14 @@ export function App() {
         <main class="container">
           <OverviewPanel />
           <div id="payments">
-          <Paid />
-          <ToPay />
-        </div>
+            <Paid />
+            <ToPay />
+          </div>
+          <div id="creditcards">
+            <Amex />
+          </div>
         </main>
+        <Footer />
         
       </>);
     
---
+++ src/components/CreditCards/Amex.jsx
@@ -0,0 +1,68 @@
+export function Amex() {
+  return (
+      <>
+      <section class="pay-card-boxes">
+      <div class="header-cards">
+        <div class="title">
+          <h3>Amex expenses</h3>
+        </div>
+        <div class="total-value">
+          <p class="negrito">€370</p>
+        </div>
+      </div>
+
+      <div class="container-creditcard">
+        <div class="description-card">
+          <div class="desc-card-left">
+            <p>26-06-2021</p>
+            <p>MediaMarket</p>
+          </div>
+          <div class="value">
+            <p>€220</p>
+          </div>
+        </div>
+        <div class="description-card">
+          <div class="desc-card-left">
+            <p>26-06-2021</p>
+            <p>Gamma</p>
+          </div>
+          <div class="value">
+            <p>€150</p>
+          </div>
+        </div>
+      </div>
+    </section><section class="pay-card-boxes">
+        <div class="header-cards">
+          <div class="title">
+            <h3>Visa Expenses</h3>
+          </div>
+          <div class="total-value">
+            <p class="negrito">€87</p>
+          </div>
+        </div>
+
+        <div class="container-creditcard">
+          <div class="description-card">
+            <div class="desc-card-left">
+              <p>26-06-2021</p>
+              <p>Kruidvat</p>
+            </div>
+            <div class="value">
+              <p>€62</p>
+            </div>
+          </div>
+          <div class="description-card">
+            <div class="desc-card-left">
+              <p>26-06-2021</p>
+              <p>Plein</p>
+            </div>
+            <div class="value">
+              <p>€25</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+
+  );
+}
