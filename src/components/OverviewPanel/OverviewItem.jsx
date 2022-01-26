2032df03590b9979267c4f745db9c22405f07338
Kelita Lamar <kelitasukita@gmail.com>
2022-01-27 00:36:19 +0100

    Add emoticon as a test

--- src/components/OverviewPanel/OverviewItem.jsx
+++ src/components/OverviewPanel/OverviewItem.jsx
@@ -2,7 +2,7 @@ export function OverviewItem(props) {
   return (
     <div className="balance-box">
         <h2>{props.title}</h2>
-        <p>{props.value}</p>
+        <p><span>&#128526;</span> {props.value}</p>
       </div>
   );
 }
