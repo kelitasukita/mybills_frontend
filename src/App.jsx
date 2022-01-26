46ecd95c033800f5c33a4b583512bc3eb101716b
Kelita Lamar <kelitasukita@gmail.com>
2022-01-27 00:30:07 +0100

    Implementing BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,29 @@ import './styles/payBoxes.scss'
 import './styles/creditCards.scss'
 import './styles/footer.scss'
 import { Header } from "./components/Header/header";
-import { OverviewPanel } from './components/OverviewPanel/OverviewPanel';
-import { Bills } from './components/Bills/Bills';
-import { CardBox } from './components/CreditCards/CardBox';
 import { Footer } from './components/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Overview } from './components/screens/Overview'
+import { Add } from './components/screens/Add'
+import { Reports } from './components/screens/Reports'
+import { Dashboard } from './components/screens/Dashboard'
 
 export function App() {
-  const cards = [
-    {brand: 'Amex', title: 'Amex', factur: 6732.89},
-    {brand: 'Visa', title: 'Visa Kelita', factur: 123.89},
-    {brand: 'Visa', title: 'Visa Eder', factur: 321.89},
-    {brand: 'NuBank', title: 'NuBank Kelita', factur: 432.89},
-    {brand: 'Maestro', title: 'Maestro Kelita', factur: 345.89},
-  ];
 
   return (
-      <>
+    <>
+      <BrowserRouter>
         <Header />
-
         <main className="container">
-          <OverviewPanel />
-          <div className="payments">
-            <Bills endpoint="/expenses/paid" title="Paid Bills"/>
-            <Bills endpoint="/expenses/unpaid" title="Bills to Pay"/>
-            {
-              cards.map((card, index) => {
-                return <CardBox key={index} data={card}/>
-              })
-            }
-          </div>
+          <Routes>
+            <Route path="/" element={<Overview />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/Add" element={<Add />} />
+          </Routes>
         </main>
-        
         <Footer />
-        
-      </>);
+      </BrowserRouter>
+    </>);
     
 }
