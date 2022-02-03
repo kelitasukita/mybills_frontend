7421864811cffe27218e9c6a2dc39e2c8a798e7b
Kelita Lamar <kelitasukita@gmail.com>
2022-02-03 17:19:09 +0100

    Implementing overview earning expense balance

--- src/components/OverviewPanel/OverviewPanel.jsx
+++ src/components/OverviewPanel/OverviewPanel.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import { OverviewItem } from "./OverviewItem";
 
-export function OverviewPanel() {
+export function OverviewPanel(props) {
+
+  const [earnings, setEarnings] = useState();
+  const [expenses, setExpenses] = useState();
+  const [balance, setBalance] = useState();
+  
+  // useEffect(funcao, arrayDeCoisasQuePodemMudarParaExecutarAFuncaoAnterior)
+
+  useEffect(() => {
+    fetch(`http://localhost:3333/overview?year=${props.year}&month=${props.month}`)
+      .then(response => response.json())
+      .then(response => {
+        setEarnings(response.earnings);
+        setExpenses(response.expenses);
+        setBalance(response.balance);
+      });
+
+  }, [props.year, props.month]);
+
   return (
     <section id="balance">
     
-      <OverviewItem title="Earnings" value="$4.299"/>
-      <OverviewItem title="Expenses" value="$3.200"/>
-      <OverviewItem title="Balance"  value="$1.300"/>
+      <OverviewItem title="Earnings" value={earnings}/>
+      <OverviewItem title="Expenses" value={expenses}/>
+      <OverviewItem title="Balance"  value={balance}/>
 
     </section>
   );
--- src/components/screens/Overview.jsx
+++ src/components/screens/Overview.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { Bills } from "../Bills/Bills";
 import { CardBox } from "../CreditCards/CardBox";
 import { OverviewPanel } from "../OverviewPanel/OverviewPanel";
 
 export function Overview() {
+
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [month, setMonth] = useState(new Date().getMonth());
+
   const cards = [
     {brand: 'Amex', title: 'Amex', factur: 6732.89},
     {brand: 'Visa', title: 'Visa Kelita', factur: 123.89},
@@ -13,11 +18,13 @@ export function Overview() {
 
   return (
     <>
-      <OverviewPanel />
+      <input type="number" onChange={(event) => setYear(event.target.value)} placeholder="2022" />
+      <input type="number" onChange={(event) => setMonth(event.target.value)} placeholder="2" />
+      <OverviewPanel year={year} month={month}/>
       <div className="payments">
             
-        <Bills endpoint="/expenses/paid" title="Paid Bills"/>
-        <Bills endpoint="/expenses/unpaid" title="Bills to Pay"/>
+        <Bills endpoint={`/expenses/paid?year=${year}&month=${month}`} title="Paid Bills"/>
+        <Bills endpoint={`/expenses/unpaid?year=${year}&month=${month}`} title="Bills to Pay"/>
         {
           cards.map((card, index) => {
             return <CardBox key={index} data={card}/>
