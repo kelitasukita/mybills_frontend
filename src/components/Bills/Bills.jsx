31a919f70dae630f053b53eb411651b62c42e66a
Kelita Lamar <kelitasukita@gmail.com>
2022-02-21 14:24:30 +0100

    Add total amount and styles

--- src/components/Bills/Bills.jsx
+++ src/components/Bills/Bills.jsx
@@ -4,18 +4,23 @@ import { BillItem } from "./BillItem";
 export function Bills(props) {
 
   const [list, setList] = useState([]);
+  const [total, setTotal] = useState([]);
 
 // O fetch é uma forma de usar um GET nativo. Para um PUT e POST preciso do AXIOS. 
   useEffect(() => { 
     fetch('http://localhost:3333' + props.endpoint) 
       .then(response => response.json())
-      .then(apiResponse => setList(apiResponse.data))
+      .then(apiResponse => { 
+        setList(apiResponse.data);
+        setTotal(apiResponse.total);
+      })
   }, [props.endpoint]);
 
   return (
       <section className="pay-card-boxes">
         <div  className="title">
           <h3>{props.title}</h3>
+          <h3 className="total">€ {total}</h3>
         </div>
         <div className="container-bills">
           {
