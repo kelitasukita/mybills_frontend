f38407028e919d3b7be8d8c0a574f47631f0ee52
Kelita Lamar <kelitasukita@gmail.com>
2022-03-16 11:53:31 +0100

    Fix bug

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, MenuTitle } from "./menuStyles";
+import { Container, MenuTitle } from './MenuStyles';
 
 const Menu: React.FC = () => {
   return (
